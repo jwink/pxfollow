35481e8527c64f7e42adfce34de3398afdebab85
jwink <jeff_winkler@yahoo.com>
2017-03-29 15:52:01 -0400

    init commit

---
+++ build_all.bat
@@ -0,0 +1,3 @@
+
+@echo off
+call ".\node_modules\.bin\browserify.cmd" .\js\ui\main.js -o .\public\js\bundle.js
---
+++ ts/app.ts
@@ -0,0 +1,20 @@
+
+import * as path from 'path';
+import * as express from 'express';
+
+let app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  next();
+});
+
+app.get('/update/:symbol', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.log(req.params);
+  res.redirect('/');
+  next();
+});
+
+
+app.listen(8080);
